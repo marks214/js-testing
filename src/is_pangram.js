3b8a798efb29d2ce148f1d38dc849b8047da046a
marks214 <aimee.oz.x@gmail.com>
2020-12-10 11:39:47 -0800

    function works, passes all tests

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,5 +1,16 @@
 const isPangram = function(text) {
+  let require = 'abcdefghijklmnopqrstuvwxyz'
+  let str = text.toLowerCase();
+  let count = require.length
 
-};
+  for(let i = 0; i < str.length; i++) {
+    if (require.includes(str[i])) {
+      count -= 1;
+    }
+    if (count === 0) { return true; }
+  }
+   return false;
+}
+
 
 module.exports = isPangram;
